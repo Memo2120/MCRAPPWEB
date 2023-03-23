43db079abee76484635dc8185f821e6e03539006
Memo2120 <a16301515@gmail.com>
2023-03-22 23:38:34 -0600

    Actualizacion

--- resources/views/privado/CRUDS/index.blade.php
+++ resources/views/privado/CRUDS/index.blade.php
@@ -11,12 +11,23 @@
 
 @section('content')
     <div class="div_scroll">
-        <button id="1" onclick="on(this.id)">Refacciones</button>
+        <select name="1" id="sel1">
+            <option value="Refacciones" selected="selected" hidden>Refacciones</option>
+            <option value="agregar"><button id="1" onclick="on(this.id)">Agregar</button></option>
+            <option value="mostrar"><button onclick="mostrar">Mostrar</button></option>
+        </select>
+        
         <button id="2" onclick="on(this.id)">Solicitud Tickets</button>
         <button id="3" onclick="on(this.id)">Usuarios</button>
         <button id="4" onclick="on(this.id)">Tecnicos</button>
         <button id="5" onclick="on(this.id)">Tickets</button>
     </div>
+
+    <div class="div_1">
+        
+        <button onclick="on">Agregar</button>
+    </div>
+    
     @include('privado/CRUDS/refaccionesCRUD')
     @include('privado/CRUDS/usersCRUD')
     @include('privado/CRUDS/soliTicketsCRUD')
--- resources/views/privado/refacciones/display.blade.php
+++
@@ -1,8 +0,0 @@
-<div id="overlay">
-    <div style="div_2">
-        <form action="">
-
-            <button onclick="off()">Cerrar</button>
-        </form>
-    </div>
-</div>
